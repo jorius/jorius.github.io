5fb7b6001260a65bb597ffafb5805132359e9f97
Jose Ríos <josed.riosc@gmail.com>
2026-04-27 16:14:40 -0500

    Fix overflow to unblock sticky positioning on all pages

--- src/pages/Read.tsx
+++ src/pages/Read.tsx
@@ -37,7 +37,7 @@ const Read = (): React.ReactElement => {
         fontFamily: 'Space Mono, monospace',
         minHeight: '100vh',
         position: 'relative',
-        overflow: 'hidden',
+        overflow: 'clip',
       }}
     >
       <BTopBar />
